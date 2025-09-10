4b64309097e1c80bdd1e439230079d21ee65a678
SirTigas <tiagocardoso2077@gmail.com>
2025-09-10 11:06:14 -0300

    alterei a cor do botão kkkkk

--- resources/views/admin/dashboard_add.blade.php
+++ resources/views/admin/dashboard_add.blade.php
@@ -18,14 +18,14 @@
         <input type="text" name="description" placeholder="Breve descrição"><br><br>
         <input type="number" name="price" step="0.01" min="1" placeholder="Valor do produto"><br><br>
         <input type="number" name="stock" min="1" placeholder="Estoque"><br><br>
-        <input type="text" name="fornecedor" placeholder="nome do fornecedor"><br><br>
+        <input type="text" name="fornecedor" placeholder="Nome do Fornecedor"><br><br>
         Gênero: <select name="category_id">
             <option value="1">1 - Homem</option>
             <option value="2">2 - Mulher</option>
             <option value="3">3 - Unissex</option>
         </select><br><br>
 
-        <button type="submit">CRIAR PRODUTO</button>
+        <button type="submit" class="btn btn-success"><i class="bi bi-plus-circle-fill"></i> <b>CRIAR PRODUTO</b></button>
     </form>
 </div>
 
