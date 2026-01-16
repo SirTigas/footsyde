53212514e0380a30b3236bf4f8ee06edd4d48aba
SirTigas <tiagocardoso2077@gmail.com>
2026-01-15 22:13:07 -0300

    ajustes finos nas views dos emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,16 +2,12 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
+{{-- @if (trim($slot) === 'Footsyde')
+<img src="https://i.ibb.co/fdfSxTnB/logo.png" class="logo" alt="logo-footsyde">
+@else --}}
 {{-- {!! $slot !!} --}}
-<img 
-    src="{{ config('app.url') . '/images/logo.png' }}"
-    class="logo"
-    alt="Footsyde"
-/>
-@endif
+<img src="https://i.ibb.co/fdfSxTnB/logo.png" alt="logo-footsyde" border="0">
+{{-- @endif --}}
 </a>
 </td>
 </tr>
